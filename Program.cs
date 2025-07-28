280402d1c2f3ace667f20485f2288c2d7721ca90
Eda Öney <oneeyyeda@gmail.com>
2025-07-28 18:43:23 +0300

    Completed

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("KDV HESAPLAMA");
+
+Console.Write("Ürün giriniz: ");
+string product = Console.ReadLine();
+
+string urun = "Çamaşır Makinesi";
+
+if (urun == product)
+{ 
+    Console.WriteLine("KDV'siz: 15000TL");
+    Console.WriteLine("KDV'si : "+ 15000 * 0.20 + "TL");
+    Console.WriteLine("Toplam: 18000TL");
+}
+if (urun != product)
+{
+    Console.WriteLine("ürün bulunmamaktadır.");
+}
